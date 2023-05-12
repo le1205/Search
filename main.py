8b086f85f12fa009b1d0497bcd1a11ce05318dcd
Joel Louis <53653635+joellui@users.noreply.github.com>
2023-05-12 09:37:36 +0530

    Creating an output file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from UserNameGen.username import userNameGen
 def main(Name):
     test = userNameGen(Name)
     print(test.gen())
+    with open("output.txt", "w") as f:
+        f.write(str(test.gen()))
 
 if __name__ == "__main__":
     main("Joel Louis")
