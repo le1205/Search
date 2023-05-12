edf1beb6f4a4ee4e7567b470009b1cbdbb238ab4
superbluestar <81801796+SuperBluestar@users.noreply.github.com>
2023-05-13 02:51:32 +0900

    basic scraping from instagram

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from UserNameGen.username import userNameGen
+from urlCheck.url_service import URLService
+
 
 def main(name):
     test = userNameGen(name)
-    print(test.gen())
     with open("output.txt", "w") as f:
-        f.write(str(test.gen()))
+        for name in test.gen():
+            print(name)
+            f.write(str(URLService.is_username_present(
+                name, ["https://instagram.com/@username"])))
+
 
 if __name__ == "__main__":
     main("Joel Louis")
